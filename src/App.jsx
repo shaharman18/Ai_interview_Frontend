87e9932da0f45b55d36b6140413ec47de28e403f
shaharman18 <shahaman604@gmail.com>
2026-04-29 11:20:29 +0530

    Initial commit for frontend

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app.routes.jsx'
+import './App.css'
+import { AuthProvider } from './features/auth/Auth.context.jsx'
+import { InterviewProvider } from './features/ai/Interview.context.jsx'
+
+
+function App() {
+
+
+  return (
+    <>
+    <AuthProvider>
+      <InterviewProvider>
+        <RouterProvider router={router}/>
+      </InterviewProvider>
+    </AuthProvider>
+    </>
+ 
+  )
+}
+
+export default App
---
+++ src/features/auth/services/auth.api.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+const API = axios.create({
+    baseURL: "http://localhost:3000/api",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    withCredentials: true
+});
+
+export async function sendRegisterOTP(email) {
+    const response = await API.post("/auth/send-otp", { email });
+    return response.data;
+}
+
+export async function registerUser({ name, email, password, otp }) {
+    const response = await API.post("/auth/register", { username: name, email, password, otp });
+    return response.data;
+}
+
+export async function loginUser({ email, password }) {
+    const response = await API.post("/auth/login", { email, password });
+    return response.data;
+}
+
+export async function logoutUser() {
+    const response = await API.post("/auth/logout", {});
+    return response.data;
+}
+
+export async function getCurrentUser() {
+    try {
+        const response = await API.get("/auth/getme");
+        return response.data;
+    } catch (error) {
+        if (!error.response || error.response.status === 401) {
+            return { user: null };
+        }
+        throw error;
+    }
+}
+
+export async function updateUserProfile({ name, email }) {
+    const response = await API.put("/auth/update-profile", { username: name, email });
+    return response.data;
+}
+
+export async function forgotPassword(email) {
+    const response = await API.post("/auth/forgot-password", { email });
+    return response.data;
+}
+
+export async function resetPassword({ email, otp, newPassword }) {
+    const response = await API.post("/auth/reset-password", { email, otp, newPassword });
+    return response.data;
+}
